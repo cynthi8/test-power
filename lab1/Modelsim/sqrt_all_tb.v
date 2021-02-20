2d7b71cf0ae9298e8293cfa48c140e4337adfb51
Erin Cold <coldegret@protonmail.com>
2021-02-19 23:06:25 -0500

    Add test result reporting to testbench
    Now each test will run from a clear state and report if it fails.

--- lab1/Modelsim/sqrt_all_tb.v
+++ lab1/Modelsim/sqrt_all_tb.v
@@ -1,5 +1,5 @@
 module sqrt_tb();
-  integer increment;
+  integer answer;
   reg start, clk, clear;
   reg [6:0] num;
   wire ready;
@@ -8,27 +8,35 @@ module sqrt_tb();
   sqrt_Top a1(start, clk, clear, num, result, ready);
 
   initial begin
-    increment = 0;
+    // Initialize
     clk = 0;
     start = 0;
     clear = 1;
-    num = 1;
-    #10; 
+    #10;
+    // Start test for sqrt(1)
     start = 1;
     clear = 0;
+    answer = 1;
+    num = answer*answer;
   end
 
   always #1 clk = ~clk;
 
   always @(posedge ready) begin
-    if(increment>=11) begin
-	increment = 0;
-	num = 1;
-	clear = 1; 
-	#10 clear = 0;
+    // Check results of last test
+    if(result != answer)
+        $display("test failed for answer=", answer);
+    clear = 1;
+    #2;
+    // Start test for next sqrt
+    clear = 0;
+    answer = answer + 1;
+    num = answer*answer;
+    
+    if(answer>11) begin
+        $display("Simulation Complete");
+        $stop;
     end
-    increment = increment + 1;
-    num = increment*increment;
   end
 
 endmodule
