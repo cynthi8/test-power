3a6bfe79469f21aa103bf2eaa4e7b545bcd52900
Erin Cold <coldegret@protonmail.com>
2021-03-13 00:01:23 -0500

    Cleanup: Remove unused folder

--- lab2/core_functional_test/run_simulation.tcl
+++ lab2/core_functional_test/run_simulation.tcl
@@ -6,7 +6,6 @@ vcom -reportprogress 300 -work work *.vhdl
 # Run Simulation
 vsim work.gcd_tb
 add wave -position end sim:/gcd_tb/*
-#add wave -position end sim:/gcd_tb/uut/TOFSM/*
 run -all
 wave zoom full
 view wave
