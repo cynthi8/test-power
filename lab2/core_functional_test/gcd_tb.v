08ecb7cdc7750adfd2ac21234fcee8dd9c7cb4fb
Erin Cold <coldegret@protonmail.com>
2021-03-12 20:24:22 -0500

    Add new tests and fix gcb_tb.v and cleanup FSM.
    FSM now explicitally assigns values in every case.
    gcb_tb.v was stuck in reset - now fixed.

--- lab2/core_functional_test/gcd_tb.v
+++ lab2/core_functional_test/gcd_tb.v
@@ -7,6 +7,7 @@ module gcd_tb();
     gcd uut(.rst(rst), .clk(clk), .go_i(go_i), .x_i(x_i), .y_i(y_i), .d_o(d_o));
 
     parameter period = 40;
+    parameter cycles_per_test = 20;
 
     initial begin
         clk = 0;
@@ -21,16 +22,40 @@ module gcd_tb();
         x_i = 0;
         y_i = 0;
         #(period);
+        rst = 0;
+        #(period);
 
+        // Test 1
         x_i = 12;
         y_i = 8;
         go_i = 1;
         #(period);
         go_i = 0;
-        #(10 * period);
-        
+        #(cycles_per_test * period);
         if (d_o != 4) begin
             $display("%0t test failed for gcd(12,8) = 4", $time);
+        end
+
+        // Test 2
+        x_i = 7;
+        y_i = 3;
+        go_i = 1;
+        #(period);
+        go_i = 0;
+        #(cycles_per_test * period);
+        if (d_o != 1) begin
+            $display("%0t test failed for gcd(7,3) = 1", $time);
+        end
+
+        // Test 3
+        x_i = 15;
+        y_i = 15;
+        go_i = 1;
+        #(period);
+        go_i = 0;
+        #(cycles_per_test * period);
+        if (d_o != 15) begin
+            $display("%0t test failed for gcd(15,15) = 15", $time);
         end
 
         $display("Simulation Complete");
--- lab2/core_functional_test/run_simulation.tcl
+++ lab2/core_functional_test/run_simulation.tcl
@@ -6,6 +6,6 @@ vcom -reportprogress 300 -work work *.vhdl
 # Run Simulation
 vsim work.gcd_tb
 add wave -position end sim:/gcd_tb/*
-add wave -position end sim:/gcd_tb/uut/*
+#add wave -position end sim:/gcd_tb/uut/TOFSM/*
 run -all
 wave zoom full
