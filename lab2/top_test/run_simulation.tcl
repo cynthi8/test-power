8dd5ac125d1a2379b50cea3012018efef787b7fc
Erin Cold <coldegret@protonmail.com>
2021-03-20 01:12:28 -0400

    Working flush test

--- lab2/top_test/run_simulation.tcl
+++ lab2/top_test/run_simulation.tcl
@@ -6,6 +6,9 @@ vlog -reportprogress 300 -work work ../../saed90nm_typ_ht_verilog/*
 # Run Simulation
 vsim work.top_tb
 add wave -position end sim:/top_tb/*
+add wave -position end  sim:/top_tb/uut/TOP_DW_tap_inst/instructions
+add wave -position end  sim:/top_tb/uut/TOP_bypass_sel
+
 run -all
 wave zoom full
 view wave
